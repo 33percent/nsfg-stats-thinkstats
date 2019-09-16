1d0362d8bb607039724bdd76e0e1c0b8ca96fc9a
sandeep <sandeep0814@gmail.com>
2019-09-16 08:42:44 +0530

    opumpkin stats

--- day1/self/all.py
+++ day1/self/all.py
@@ -1,15 +1,16 @@
 import sys
 import modules.survey as survey
+import modules.thinkstats as gen_stats
 
-# 2.3.1
+# 1.3.1
 print('-------------------------')
-print('example 2.3.1')
+print('example 1.3.1')
 table = survey.Pregnancies()
 table.ReadRecords()
 df = table.records
 print ('Number of pregnancies', len(df))
 
-# exercise 2.3
+# exercise 1.3
 
 '''available methods
 table.GetFilename() - to retrieve all methods
@@ -28,18 +29,18 @@ table.records - the actual data
             ('finalwgt', 423, 440, float),
             ]
 '''
-# 2.3.2
+# 1.3.2
 print('-------------------------')
-print('example 2.3.2')
+print('example 1.3.2')
 print('live births are where outcomes are 1')
 print('No of live births: ',sum(1 for x in table.records if x.outcome == 1))
 
 
 print('-------------------------')
 
-# 2.3.3
+# 1.3.3
 
-print('example 2.3.3')
+print('example 1.3.3')
 def getbirthOrders(data):
     firsts = []
     lasts = []
@@ -57,7 +58,7 @@ print('No of non-first babies',len(otherbabies))
 print('-------------------------')
 
 
-print('example 2.3.4')
+print('example 1.3.4')
 def averagepreg(data):
     total = sum(x.prglength for x in data if x.outcome == 1)
     count = len(data)
@@ -69,3 +70,16 @@ print('Average weeks of first born babies',firstBornAveragePreg)
 print('Average weeks of non-first born babies',otherBornAveragePreg)
 print('difference between both of them is ',(firstBornAveragePreg - otherBornAveragePreg) * 7)
 print('-------------------------')
+
+
+
+print('2.1')
+def pumpkinStats(data):
+    print('Mean - ',gen_stats.Mean(data))
+    print('Variance - ',gen_stats.Var(data))
+    print('standard deviation - ',gen_stats.Standard_deviation(data))
+
+pumpkin_data = [3,3,591]
+pumpkinStats(pumpkin_data)
+
+
